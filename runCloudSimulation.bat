52987bca180a2fbd7cf9723cfda31828abc45faf
Tom Carroll <tomalancarroll@gmail.com>
2013-11-27 17:40:44 -0500

    Added option for which component to increase in the simulations: Clients, Routers, or Both

--- runCloudSimulation.bat
+++ runCloudSimulation.bat
@@ -7,19 +7,30 @@ javac -d bin -classpath "lib\opencsv-2.3.jar;" src\simulation\*.java src\simulat
 echo Welcome, this will run the simulation.
 :: Set the congestion avoidance algorithm (Tahoe, Reno, or NewReno)
 set /P congestionAvoidanceAlgorithm= Please enter the congestion avoidance algorithm (Tahoe, Reno, or NewReno):
+:: Let the user pick to increase the number of clients, routers, or both
+set /P increaseOption= Please enter the which network elements to increase (Clients, Routers, or Both):
 :: Set the number of clients
-set /a maxClients= 100
+set /a maxClients= 1000
+set /a maxRouters= 100
 
 set /a numClients = 0
-set /a numRouters = 1
+set /a numRouters = 0
 
 setlocal enabledelayedexpansion enableextensions
 
 :: Start looping here while increasing the jar pars
 :: Loop from 0 to maxClients
-for /L %%i in (1 1 %maxClients%) do (
-    set /a numClients = !numClients! + 1
-::    set /a numRouters = !numRouters! + 1
+for /L %%i in (1 1 100) do (
+    if "%increaseOption%" == "Clients" (
+        set /a numClients = !numClients! + 10
+        set /a numRouters = 1
+    ) else if "%increaseOption%" == "Routers" (
+        set /a numClients = 1
+        set /a numRouters = !numRouters! + 1
+    ) else if "%increaseOption%" == "Both" (
+        set /a numClients = !numClients! + 10
+        set /a numRouters = !numRouters! + 1
+    )
     java -classpath "bin;lib\opencsv-2.3.jar;" simulation.Simulator !congestionAvoidanceAlgorithm! 100 Cloud 6244 65536 !numClients! !numRouters!
 
     :: The two lines below are used for testing
